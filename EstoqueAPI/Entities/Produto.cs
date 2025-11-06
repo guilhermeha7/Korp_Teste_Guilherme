4b565636afa8b0b57ccc2a268591844cdf2db199
Guilherme Henrique do Amaral <guilhermeha77@outlook.com>
2025-11-06 10:22:36 -0300

    Projeto criado

---
+++ EstoqueAPI/Entities/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace EstoqueAPI.Entities
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Saldo { get; set; } //quantidade disponível em estoque
+    }
+}
